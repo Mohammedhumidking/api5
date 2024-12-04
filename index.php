ed49eccad452442551f3069ac52aa0d330912464
Mohammedhumidking <160168150+Mohammedhumidking@users.noreply.github.com>
2024-12-04 20:23:29 +0300

    api5

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+ require_once('api.php');
+ $API = new RouterosAPI();
+
+$ip = strip_tags($_POST['ip']);
+$user = strip_tags($_POST['user']);
+$password = strip_tags($_POST['password']);
+
+if ($API->connect("$ip", "$user", "$password")) {
+    $respones = [
+        "status"=> true,
+        "msg"=>"good"
+    ];
+
+    print_r(json_encode($respones));
+
+} else {
+   
+
+    $respones = [
+        "status"=> false,
+        "msg"=>"no user name or password"
+    ];
+    print_r(json_encode($respones));
+}
+
+
+
+?>
